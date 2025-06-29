d347793c339ac8c9b99e8d5457a9357da092494a
georgos9433 <georgos9433@gmail.com>
2025-06-29 02:40:26 +0200

    up

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3659253874"
+    serviceWorkerVersion: "1980186715"
   }
 });
