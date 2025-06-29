9efd60a8c7ca9333a7afe8047f8532d520590449
georgos9433 <georgos9433@gmail.com>
2025-06-29 02:36:41 +0200

    pausa tra i due player

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -46,14 +46,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "c906df500b3736fa890847f094f958d8
 "canvaskit/skwasm_st.wasm": "56c3973560dfcbf28ce47cebe40f3206",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
-"flutter_bootstrap.js": "873984ad059546c17c21c79d0ac88b88",
+"flutter_bootstrap.js": "095e52d4dd421596ffbbe1354436f79e",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "7333d051beed12b49610855183fd5b79",
 "/": "7333d051beed12b49610855183fd5b79",
-"main.dart.js": "d4bc56de5a5fd4fc0fcb6b76da76c893",
+"main.dart.js": "a7b7fd088b363cae8a782bef2c05b23d",
 "manifest.json": "97b4581dbf32330b6b76b83b9cbaaa4d",
 "version.json": "d50d92607e1db784d59728242d3a00e5"};
 // The application shell files that are downloaded before a service worker can
